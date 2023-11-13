07c1316aceb618698b0a3bf345b491d574b63a3b
Momo <mozeart@hotmail.com>
2023-11-13 22:15:53 +0100

    zap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, request, render_template
+from flask import Flask, render_template_string, request, render_template,escape
 import os
 import pymssql
 
